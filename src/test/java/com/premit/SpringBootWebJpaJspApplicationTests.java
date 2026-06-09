16b22da2c22606618519568078af0f488ca4393e
mutha <mutha@192.168.1.35>
2026-06-09 18:03:40 +0530

    Initialize Project creation spring-boot-web-jpa-jsp.

---
+++ src/test/java/com/premit/SpringBootWebJpaJspApplicationTests.java
@@ -0,0 +1,13 @@
+package com.premit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebJpaJspApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
